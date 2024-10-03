b925a65007f359b53d7b08eb3e062fee8403bb07
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-10-04 01:28:43 +0530

    update

--- src/app/budget-planner/budget-planner-routing.module.ts
+++ src/app/budget-planner/budget-planner-routing.module.ts
@@ -10,20 +10,20 @@ import { HistoryComponent } from './history/history.component';
 import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },  // Redirect base URL to login page
-  { path: 'login', component: LoginComponent },  // Define login route
-  { path: 'side-nav', component: SideNavComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirect to login when the app starts
+  { path: 'login', component: LoginComponent }, // Login route
+  { path: 'side-nav', component: SideNavComponent }, 
+  { path: 'dashboard', component: DashboardComponent }, 
   { path: 'income', component: IncomeComponent },
   { path: 'expense', component: ExpenseComponent },
-  { path: 'todo', component: TodoComponent },
-  { path: 'history', component: HistoryComponent },
+  { path: 'todo', component: TodoComponent }, 
+  { path: 'history', component: HistoryComponent }, 
   { path: 'profile', component: ProfileComponent },
-  { path: '**', redirectTo: 'login', pathMatch: 'full' }  // Catch-all route to redirect to login if no other routes match
+  { path: '**', redirectTo: 'login', pathMatch: 'full' } // Wildcard route to handle undefined routes
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],  // Use forRoot for the main routing module
+  imports: [RouterModule.forRoot(routes)], // Use forRoot since it's the main routing module
   exports: [RouterModule]
 })
 export class BudgetPlannerRoutingModule { }
